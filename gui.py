ab65b381cc795c2cd8a421051689d7b0767a4ac8
Quentin Goss <gossq@my.erau.edu>
2023-05-24 13:34:02 -0400

    Statistics tab graphs.

--- gui.py
+++ gui.py
@@ -162,19 +162,24 @@ class ScenicSUMOGui:
         # Load params and scores
         # params_fn = self.values["params-fn"]
         # scores_fn = self.values["scores-fn"]
-        params_fn = "out\parameter and scores\PARAM2t120230425231600.tsv"
-        scores_fn = "out\parameter and scores\SCORE2t120230425231600.tsv"
+        params_fn = "parameter and scores/PARAM2t120230425231600.tsv"
+        scores_fn = "parameter and scores/SCORE2t120230425231600.tsv"
         params_df = pd.read_csv(params_fn, sep="\t")        
         scores_df = pd.read_csv(scores_fn, sep="\t")
 
+        # Drop ped crossing
+        if "ped_ego_wait_at_xing_event" in scores_df.columns:
+            scores_df = scores_df\
+                .drop(columns="ped_ego_wait_at_xing_event", axis=1)
+
         # Parse run id
-        run_id = scores_fn.split("\\")[-1].lower()[5:-4]
+        run_id = scores_fn.split("/")[-1].lower()[5:-4]
 
         # Stat directory
         run_stat_dir = "%s/%s" % (self.stats_dir, run_id)
         if not os.path.exists(run_stat_dir):
             os.makedirs(run_stat_dir)
-        
+
         # Build the TSC
         ops = {
             ">"  : operator.gt,
@@ -194,6 +199,7 @@ class ScenicSUMOGui:
             return
         
         if self.values["tsc_includer_1"] == "":
+            score_str = "%s %s %.3f" % (f0, c0, v0)
             tsc = lambda s : ops[c0]( s[f0], v0)
         else:
             i1 = self.values["tsc_includer_1"]
@@ -203,11 +209,12 @@ class ScenicSUMOGui:
                 v1 = float(re.findall(r"\d+\.*\d*",self.values["tsc_value_1"])[0])
             except IndexError:
                 return
+            score_str = "%s %s %.3f\n%s %s %s %.3f" \
+                % (f0, c0, v0, i1, f1, c1, v1)
             tsc = lambda s : ops[i1]( ops[c0]( s[f0], v0), ops[c1]( s[f1], v1))
         
         # Get indices of score with parameters
         tsc_indices = scores_df[scores_df.apply(tsc, axis=1)].index
-            
 
         # Scores over time
         tsc_indices_set = set(tsc_indices)
@@ -218,19 +225,51 @@ class ScenicSUMOGui:
                 n += 1
             tsc_count.append(n)
             continue
+
+        # Summary of test
+        params_df.describe()\
+            .to_csv("%s/params_describe.csv" % run_stat_dir, index=False)
+        scores_df.describe()\
+            .to_csv("%s/scores_describe.csv" % run_stat_dir, index=False)
+        
+        tsc_params_df = params_df.iloc[tsc_indices]
+        tsc_params_df.to_csv("%s/tsc_params.tsv" % run_stat_dir, sep="\t")
+        tsc_params_df.describe()\
+            .to_csv("%s/tsc_params_describe.csv" % run_stat_dir, index=False)
+        
+        tsc_scores_df = scores_df.iloc[tsc_indices]
+        tsc_scores_df.to_csv("%s/tsc_scores.tsv" % run_stat_dir, sep="\t")
+        tsc_scores_df.describe()\
+            .to_csv("%s/tsc_scores_describe.csv" % run_stat_dir, index=False)
+
+    
+        # Histograms
+        df_map = {
+            "params" : params_df,
+            "scores" : scores_df,
+            "tsc_params" : tsc_params_df,
+            "tsc_scores" : tsc_scores_df
+        }
+        for key, val in df_map.items():
+            val.hist(color="black")
+            plt.tight_layout()
+            plt.savefig("%s/%s_hist.png" % (run_stat_dir, key))
+            plt.clf()
+            plt.close()
+
+
         
         # Make the plot
-        # x = [i for i in range(len(tsc_count))]
-        # y = tsc_count
+        x = [i for i in range(len(tsc_count))]
+        y = tsc_count
         # plt.figure(figsize=(5,5))
-        # ax = plt.axes()
-        # ax.plot(x, y, color="black")
-        # ax.set_xlabel("# Tests")
-        # ax.set_ylabel("%s > 0" % score_id)
-        # ax.set_title("Target Scenarios Located")
-        # plt.tight_layout()
-        # plt.savefig("%s/%s_over_time.png" % (self.out_dir, score_id))
-
+        ax = plt.axes()
+        ax.plot(x, y, color="black")
+        ax.set_xlabel("# Tests")
+        ax.set_ylabel(score_str)
+        ax.set_title("Target Scenarios Located")
+        plt.tight_layout()
+        plt.show()
         return
 
     def init_window(self):
@@ -306,7 +345,7 @@ class ScenicSUMOGui:
                     file_types = (("Tab Seperated Value", ("*.tsv")),),
                 )
             ],
-            [sg.Text("Target Score Classification")],
+            [sg.Text("Target Score Classification (TSC)")],
             self.tsc_row(0, True),
             self.tsc_row(1),
             [sg.Button("Generate", key="generate-scores")]
@@ -333,7 +372,7 @@ class ScenicSUMOGui:
             ),
             sg.Combo(
                 values=["e_brake_partial", "e_brake_full", "e_stop", 
-                    "collision", "ped_ego_wait_at_xing_event"],
+                    "collision"],
                 readonly=True,
                 size=15,
                 default_value="e_brake_partial",
